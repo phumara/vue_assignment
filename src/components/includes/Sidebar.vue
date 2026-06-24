cbd2359f3463ffc8022d9c85930b6f7a17c5b6fb
phumara <mot.phum@gmail.com>
2026-06-24 09:51:05 +0700

    change name

--- src/components/includes/Sidebar.vue
+++ src/components/includes/Sidebar.vue
@@ -16,7 +16,7 @@
                     <img :src="avatar" class="img-circle elevation-2" alt="User Image" />
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
+                    <a href="#" class="d-block">Phum MOT</a>
                 </div>
             </div>
 
