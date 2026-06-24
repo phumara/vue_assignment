1f77f5e8957bce0b6b40ef77ea7567978da5abc3
phumara <mot.phum@gmail.com>
2026-06-24 09:54:43 +0700

    vue_assigment

--- src/components/auth/SignUp.vue
+++ src/components/auth/SignUp.vue
@@ -56,14 +56,14 @@
                         </div>
                     </form>
 
-                    <div class="social-auth-links text-center mt-2 mb-3">
+                    <!-- <div class="social-auth-links text-center mt-2 mb-3">
                         <a href="#" class="btn btn-block btn-primary">
                             <i class="fab fa-facebook mr-2"></i> Sign up using Facebook
                         </a>
                         <a href="#" class="btn btn-block btn-danger">
                             <i class="fab fa-google-plus mr-2"></i> Sign up using Google+
                         </a>
-                    </div>
+                    </div> -->
 
                     <router-link to="/signin" class="text-center">Already have an account</router-link>
                 </div>
